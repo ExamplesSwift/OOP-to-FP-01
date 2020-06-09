65905c059a072e8a26fc2e421b4a14d16a3b2fed
Henry Ambicho T <yiperu@gmail.com>
2020-06-08 23:25:27 -0400

    We remove all code with OOP, this become only with function

--- OOP-to-FP-01.playground/Contents.swift
+++ OOP-to-FP-01.playground/Contents.swift
@@ -13,35 +13,12 @@ func logToWindow(_ msg: String) {
   print(msg)
 }
 
-protocol Logger {
-  func print(_ msg: String)
-}
-
-class ConsoleLogger : Logger {
-  func print(_ msg: String) {
-    logToConsole(msg)
-  }
-}
-
-class FileLogger : Logger {
-  func print(_ msg: String) {
-    logToFile(msg)
-  }
-}
-
-class WindowLogger : Logger {
-  func print(_ msg: String) {
-    logToWindow(msg)
-  }
-}
-
-// Now we have log before show the result
-func getValue(from number: Int, loggerType: Logger) -> String {
+func getValue(from number: Int, logger: (String) -> Void) -> String {
   // First determine the result
   let result = "Something \(number)"
-  loggerType.print(result)
+  logger(result)
   
   return result
 }
 
-getValue(from: 8, loggerType: ConsoleLogger())
+getValue(from: 8, logger: logToConsole)
